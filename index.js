50beacaa10a9b91932b6d2631d16775f9fa2467d
Ritchie Martori <skawful@gmail.com>
2012-12-10 11:32:02 -0800

    fixed setResult()

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ EventResource.prototype.clientGeneration = true;
 EventResource.prototype.handle = function (ctx, next) {
   var parts = ctx.url.split('/').filter(function(p) { return p; });
 
+  var result = {};
+
   var domain = {
       url: ctx.url
     , parts: parts
     , query: ctx.query
     , body: ctx.body
-    , 'this': {}
-    , result: {}
+    , 'this': result
     , setResult: function(val) {
-      domain.result = val;
+      result = val;
     }
   };
 
@@ -35,7 +36,7 @@ EventResource.prototype.handle = function (ctx, next) {
     });
   } else if (ctx.method === "GET" && this.events.get) {
     this.events.get.run(ctx, domain, function(err) {
-      ctx.done(err, domain.result);
+      ctx.done(err, result);
     });
   } else {
     next();
